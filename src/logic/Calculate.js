8de8511eee4f97b5b9c42b99f64d7eb4b187e586
DanielJBailey <Dbailey0210@gmail.com>
2018-07-22 11:23:41 -0500

    finish delete key

--- src/logic/Calculate.js
+++ src/logic/Calculate.js
@@ -6,17 +6,27 @@ export default function Calculate(obj, buttonName) {
             result:  0,
             input:  [],
             calc: []
-        }
+        };
     }
 
     //Numeric inputs
+    //test if button is numeric
     if(!isNaN(buttonName)) {
+        //return updated string
         return {
             input: obj.input + buttonName
         }
     }
 
     if(buttonName === "DEL") {
+        //get current input string
+        let newInput = obj.input;
+        //remove last character from string
+        newInput = newInput.substring(0, newInput.length -1);
+        //return new string once last character has been removed
+        return {
+            input: newInput
+        };
     }
 
 
